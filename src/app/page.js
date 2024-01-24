5c9b3ae8da552ccb108c65fc4c13ce29e63fa84b
Minh Bach <minhbach@Minhs-MacBook-Air.local>
2024-01-23 19:20:44 -0800

    first commit

---
+++ src/app/page.js
@@ -0,0 +1,26 @@
+import Image from "next/image";
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <main>
+    <h1> Minh Bach </h1>
+    <section>
+    <h2>Skills</h2>
+    <ul>
+    <li>Java</li>
+    <li>JavaScript</li>
+    <li>HTML/CSS</li>
+    <li>Next.js</li>
+    </ul>
+    </section>
+    <section>
+    <ul>
+    <li>
+    <h2>Project 1
+    </h2></li>
+    </ul>
+    </section>
+    </main>
+  );
+}
